13ef0f551f654da7bc00be8e60b173afd40f6d1f
FlogWog <flogsworth@gmail.com>
2025-03-14 20:46:46 -0700

    at home, terminal displays frankenstein

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        split_words = f.read()
+    return split_words
+
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    print(book_text)
+
+main()
