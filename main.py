1396d002fc694437a851090244d353aac8fdd2c9
FlogWog <flogsworth@gmail.com>
2025-03-14 21:00:19 -0700

    at home, stats.py and ignore update

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import word_count
+
 def get_book_text(filepath):
     with open(filepath) as f:
         split_words = f.read()
@@ -5,6 +7,7 @@ def get_book_text(filepath):
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
-    print(book_text)
+    num_words = word_count(book_text)
+    print(f"{num_words} words found in the document")
 
 main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def word_count(book_text):
+    word_list = book_text.split()
+    return len(word_list)
