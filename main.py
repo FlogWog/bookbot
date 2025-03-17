13afb731d293fd2ffb92fedeee6abb2d727fbb52
FlogWog <flogsworth@gmail.com>
2025-03-16 20:37:19 -0700

    sorts text by descending value

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from stats import word_count
+from stats import char_counter
+from stats import sorter
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,6 +10,22 @@ def get_book_text(filepath):
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     num_words = word_count(book_text)
-    print(f"{num_words} words found in the document")
+    char_count = char_counter(book_text)
+    sorted_char =  sorter(char_count)
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")  
+
+    for char_dict in sorted_char:
+        char = char_dict["character"]
+        count = char_dict["count"]
+
+        if char.isalpha():
+            print(f"{char}: {count}")
+
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,26 @@
 def word_count(book_text):
     word_list = book_text.split()
     return len(word_list)
+
+def char_counter(book_text):
+    lower_case = book_text.lower()
+    char_dict = {}
+    for letter in lower_case:
+        if letter in char_dict:
+            char_dict[letter] += 1
+        else:
+            char_dict[letter] = 1
+
+    return char_dict
+
+def sorter(dictionary):
+    sorted_list = [] 
+    for char, count in dictionary.items():
+        sorted_list.append({"character": char, "count": count})
+
+    def sort_on(dict_item):
+        return dict_item["count"]
+
+    sorted_list.sort(reverse=True, key=sort_on)
+
+    return sorted_list
